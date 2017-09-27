f0c99dea81e052acf0312885366aaeea6eee7e9d
siliingamedev <siliingamedev@gmail.com>
2017-09-27 17:54:01 +0300

    Добавьте файлы проекта.

---
+++ Lesson4_r_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_r_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
