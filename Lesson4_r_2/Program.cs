e6af10ba873ce8f7d785b4e8514e0941c9dc0d8d
siliingamedev <siliingamedev@gmail.com>
2017-09-28 04:11:31 +0300

    Добавьте файлы проекта.

---
+++ Lesson4_r_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_r_2
+{
+    class Program
+    {
+        static void Print(int n, int[] a)
+        {
+            for (int i = 0; i < n; i++) Console.Write("{0} ",a[i]);
+            Console.WriteLine();
+        }
+        static void Change(int n, int[] a)
+        {
+            for (int i = 0; i < n; i++)
+                if (a[i] > 0) a[i] = 0;
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 0, -1, -2, 3, 4, 5, -6, -7, -8, -9 };
+            Print(10, myArray);
+            Change(10, myArray);
+            Print(10, myArray);
+            Console.ReadKey();
+        }
+    }
+}
